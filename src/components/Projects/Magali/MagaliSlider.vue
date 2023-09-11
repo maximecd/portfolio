a95f4bfd956756fa820633cf9e55fb3b30ac615a
Maxime Condaminat <me@maximecd.dev>
2023-09-12 00:55:07 +0200

    vue slides

---
+++ src/components/Projects/Magali/MagaliSlider.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import { onMounted } from 'vue';
+
+interface Image {
+  src: string
+}
+
+interface Props {
+  images: Image[]
+}
+
+const props = defineProps<Props>()
+
+onMounted(() => {
+  console.log(props.images)
+})
+
+</script>
+
+<template>
+  <div>
+    <swiper>
+      <swiper-slide v-for="img in props.images">
+        <a href="#" target="_blank">
+          <img :src="img.src" alt="Galerie Magali Martin" />
+        </a>
+      </swiper-slide>
+    </swiper>
+
+  </div>
+</template>
